cb5df05cce38b156463d0a2612cfa4fb507e2d70
DmitryMyshnev <57642271+DmitryMyshnev@users.noreply.github.com>
2021-07-18 12:39:15 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,37 @@ public class Main {
         myArrayList.add(2);
         myArrayList.add(3);
         myArrayList.add(4);
-        myArrayList.add(5);
-
-        myArrayList.remove(4);
-        myArrayList.clear();
-        for (int i = 0; i < myArrayList.getSize() ; i++) {
-            System.out.println(myArrayList.get(i));
+        for (int i = 0; i < myArrayList.size(); i++) {
+            System.out.print(myArrayList.get(i) + " ");
         }
+        System.out.println();
+       myArrayList.remove(3);
+        for (int i = 0; i < myArrayList.size(); i++) {
+            System.out.print(myArrayList.get(i) + " ");
+        }
+        System.out.println();
+        myArrayList.clear();
+        System.out.println("Size is " + myArrayList.size());
+        System.out.println("--------------------------");
 
+        MyLinkedList<String> myLinkedList = new MyLinkedList<>();
 
-
-
-
+        myLinkedList.add("1");
+        myLinkedList.add("2");
+        myLinkedList.add("3");
+        myLinkedList.add("4");
+        myLinkedList.add("5");
+        for (int i = 0; i < myLinkedList.size(); i++) {
+            System.out.print(myLinkedList.get(i) + " ");
+        }
+        System.out.println();
+        myLinkedList.remove(1);
+        for (int i = 0; i < myLinkedList.size(); i++) {
+            System.out.print(myLinkedList.get(i) + " ");
+        }
+        System.out.println();
+        myLinkedList.clear();
+        System.out.println("Size is " + myLinkedList.size());
 
     }
 }
